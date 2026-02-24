01cb7d3339ef8e4328fa110b22cac610da0e3b0f
hiremystudios agency <hiremystudiosaus@gmail.com>
2026-02-24 15:30:46 +0530

    seo: finalized meta tag management and removed index.html duplicates

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -9,28 +9,28 @@ const SEO = ({ title, description, path }) => {
         <Helmet>
             {/* Primary Meta Tags */}
             <title>{fullTitle}</title>
-            <meta name="title" content={fullTitle} />
-            <meta name="description" content={description} />
+            <meta name="title" content={fullTitle} data-rh="true" />
+            <meta name="description" content={description} data-rh="true" />
 
             {/* Open Graph / Facebook */}
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content={url} />
-            <meta property="og:title" content={fullTitle} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content="https://hs-times.com/hero-bg.png" />
+            <meta property="og:type" content="website" data-rh="true" />
+            <meta property="og:url" content={url} data-rh="true" />
+            <meta property="og:title" content={fullTitle} data-rh="true" />
+            <meta property="og:description" content={description} data-rh="true" />
+            <meta property="og:image" content="https://hs-times.com/hero-bg.png" data-rh="true" />
 
             {/* Twitter */}
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content={url} />
-            <meta property="twitter:title" content={fullTitle} />
-            <meta property="twitter:description" content={description} />
-            <meta property="twitter:image" content="https://hs-times.com/hero-bg.png" />
+            <meta property="twitter:card" content="summary_large_image" data-rh="true" />
+            <meta property="twitter:url" content={url} data-rh="true" />
+            <meta property="twitter:title" content={fullTitle} data-rh="true" />
+            <meta property="twitter:description" content={description} data-rh="true" />
+            <meta property="twitter:image" content="https://hs-times.com/hero-bg.png" data-rh="true" />
 
             {/* Keywords */}
-            <meta name="keywords" content="hs times, education, international courses, domestic courses, engineering college, higher education, university" />
+            <meta name="keywords" content="hs times, education, international courses, domestic courses, engineering college, higher education, university" data-rh="true" />
 
             {/* Canonical */}
-            <link rel="canonical" href={url} />
+            <link rel="canonical" href={url} data-rh="true" />
         </Helmet>
     );
 };
